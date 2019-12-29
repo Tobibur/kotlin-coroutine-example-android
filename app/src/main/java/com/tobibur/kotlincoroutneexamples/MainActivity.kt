40bb7345246c758bd140be59f23fbaa2cafd2f50
Tobibur Rahman <tobiburrahman786@gmail.com>
2019-12-29 18:43:12 +0530

    deferred as a Job

--- app/src/main/java/com/tobibur/kotlincoroutneexamples/MainActivity.kt
+++ app/src/main/java/com/tobibur/kotlincoroutneexamples/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var deferred: Deferred<Int>
 
     companion object {
         private const val TAG = "MainActivity"
@@ -24,24 +23,28 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val job = CoroutineScope(IO).launch {
-            downloadUserData()
+        CoroutineScope(IO).launch {
+            deferred = async { downloadUserData() }
+
+            withContext(Main) {
+                textView.text = "Downloaded data = ${deferred.await()}"
+            }
         }
 
         button.setOnClickListener {
-            if(job.isActive)
-                job.cancel()
+            if(deferred.isActive)
+                deferred.cancel()
         }
 
         button2.setOnClickListener {
             when {
-                job.isActive -> {
+                deferred.isActive -> {
                     textView2.text = "Active"
                 }
-                job.isCancelled -> {
+                deferred.isCancelled -> {
                     textView2.text = "Cancelled"
                 }
-                job.isCompleted -> {
+                deferred.isCompleted -> {
                     textView2.text = "Completed"
                 }
                 else -> textView2.text = "Not Running"
@@ -49,14 +52,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun downloadUserData() {
+    private suspend fun downloadUserData(): Int {
         Log.d(TAG, "onCreate: Running on the ${Thread.currentThread().name}")
+        var data = 0
         for (i in 0..30) {
-            withContext(Main) {
+            withContext(Main){
                 textView.text = "Running $i on the ${Thread.currentThread().name}"
             }
             delay(2000)
+            data += i+2000
         }
-
+        return data
     }
 }
